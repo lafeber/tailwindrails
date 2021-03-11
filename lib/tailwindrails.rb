f782cdafb578e26054bf4b1d4f2e1c8b9aa77498
Martijn Lafeber <martijn@coupling.nl>
2021-03-11 09:38:39 +0100

    First commit

---
+++ lib/tailwindrails.rb
@@ -0,0 +1,6 @@
+require "tailwindrails/version"
+
+module Tailwindrails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tailwindrails/version.rb
@@ -0,0 +1,3 @@
+module Tailwindrails
+  VERSION = "0.1.0"
+end
